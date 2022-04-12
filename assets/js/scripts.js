a38868714998604964027144d15e8e5f47c02ae3
Stephanie <101684009+schaparro08@users.noreply.github.com>
2022-04-12 10:30:11 -0400

    addition of event listener for submit button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,28 @@
-const apiKey = "AIzaSyD1mZtud8Jal7Szr9WhsfOcw_DI4coig-E"
+const apiKey = "AIzaSyD1mZtud8Jal7Szr9WhsfOcw_DI4coig-E";
+const startBtn = document.querySelector("#getStarted");
+const submitBtn = document.querySelector()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// submitBtn.addEventListener("click",getInfo())
