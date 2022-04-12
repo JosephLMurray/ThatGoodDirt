5d053011c6bc3f439203e400b58b8333934ca5df
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-12 11:24:10 -0400

    geoCode add, beginning of getHikingTrails

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,33 @@
-
-
-        
+const apiKey = 'AIzaSyD1mZtud8Jal7Szr9WhsfOcw_DI4coig-E'       
 // This calls the API, just update the url to have your key's name.
-async function fetchKey() {
-const apiKey = 'https://yorkieportunus.herokuapp.com/store/GoogleAPI'
-const response = await fetch(apiKey);
-const key = await response.json();
-return key;
-}
-// Call this wherever you need your key.
-fetchKey().then((key) => {
-    secretKey = key.apiKey;
-    console.log(secretKey);
+const fetchKey = async() => {
+    const apiKey = 'https://yorkieportunus.herokuapp.com/store/GoogleAPI'
+    const response = await fetch(apiKey);
+    const key = await response.json();
+    return key;
+    }
+    // Call this wherever you need your key.
+    fetchKey().then((key) => {
+        secretKey = key.apiKey;
 });
-      
-    
+
+const getGeoCode = addy => {
+    const geoAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${addy}&key=${apiKey}`
+    fetch(geoAPI)
+        .then((response) => {
+            if (response.ok) {
+                console.log(response);
+            response.json().then((data) => {
+                console.log(data)});
+            } else {
+            console.error(response.statusText);
+            }
+        })
+        .catch((error) => {
+           console.error('Unable to connect to Open Weather', error);
+        });
+};
+
+const getHikingTrails = (lat, lon) => {
+
+}
