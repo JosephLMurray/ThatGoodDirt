b8c62e4b82e8b86bea070c1423b1be48625de5c2
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-12 09:43:52 -0400

    added portunus key hider to code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,17 @@
-const apiKey = "AIzaSyD1mZtud8Jal7Szr9WhsfOcw_DI4coig-E"
+
+
+        
+// This calls the API, just update the url to have your key's name.
+async function fetchKey() {
+const apiKey = 'https://yorkieportunus.herokuapp.com/store/GoogleAPI'
+const response = await fetch(apiKey);
+const key = await response.json();
+return key;
+}
+// Call this wherever you need your key.
+fetchKey().then((key) => {
+    secretKey = key.apiKey;
+    console.log(secretKey);
+});
+      
+    
