10cc9a212dc8997435c5a80a43839c28e9224c38
Rene Ortega <reneortega2468@gmail.com>
2022-04-12 14:07:03 -0400

    form hide

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,19 +2,20 @@ const apiKey = "AIzaSyD1mZtud8Jal7Szr9WhsfOcw_DI4coig-E";
 const startBtn = document.querySelector("#getStarted");
 const submitBtn = document.getElementById('#submitBtn')
 const getStarted = document.getElementById("getStarted");
-
-
-
-
-
-
-
-
-
-
-
-
+// const formBlock = document.getElementById('#iBlock);
 const formBlock = $("#iBlock"); 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 getStarted.addEventListener('click', (e) => {
