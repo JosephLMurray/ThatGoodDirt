60d7a00351b8b65bf201f7382a21cc143344ea82
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-12 13:18:11 -0400

    geoCode add, beginning of getHikingTrails

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-const apiKey = 'AIzaSyD1mZtud8Jal7Szr9WhsfOcw_DI4coig-E'       
+let secretKey;  
 // This calls the API, just update the url to have your key's name.
 const fetchKey = async() => {
     const apiKey = 'https://yorkieportunus.herokuapp.com/store/GoogleAPI'
@@ -12,22 +12,38 @@ const fetchKey = async() => {
 });
 
 const getGeoCode = addy => {
-    const geoAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${addy}&key=${apiKey}`
+    const geoAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${addy}&key=${secretKey}`
     fetch(geoAPI)
         .then((response) => {
             if (response.ok) {
                 console.log(response);
             response.json().then((data) => {
-                console.log(data)});
+                const coords = data.results[0].geometry.location;
+                getHikingTrails(coords.lat, coords.lng);})
             } else {
             console.error(response.statusText);
             }
         })
         .catch((error) => {
-           console.error('Unable to connect to Open Weather', error);
+           console.error('Unable to connect to Google Maps', error);
         });
 };
 
 const getHikingTrails = (lat, lon) => {
-
-}
+    const radius = 5000;
+    
+    const placesAPI = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=hiking trails&location=${lat} ${lon}&radius=${radius}&key=${secretKey}`
+    fetch(placesAPI)
+        .then((response) => {
+            if (response.ok) {
+                console.log(response);
+            response.json().then((data) => {
+                console.log(data);;})
+            } else {
+            console.error(response.statusText);
+            }
+        })
+        .catch((error) => {
+           console.error('Unable to connect to Google Maps', error);
+        });
+};
