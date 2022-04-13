8c4430d1a5a4822ad83944cd99957b2a2a43ea49
Rene Ortega <reneortega2468@gmail.com>
2022-04-13 14:26:34 -0400

    styling on cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -75,7 +75,7 @@ const getGeoCode = addy => {
             }
         })
         .catch((error) => {
-          console.error('Unable to connect to Google Maps', error);
+        console.error('Unable to connect to Google Maps', error);
         });
 };
 
@@ -105,37 +105,30 @@ const createCards = (results) => {
     let siteDeck = '';
     document.getElementById('happyTrails').innerHTML = siteDeck;
     for (let i = 0; i < results.length; i++) {
-        siteDeck += `<div class="basis-1/3">` +
-                    `<div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">` +
-                    `<a href="#">` +
-                    `<img class="p-8 rounded-t-lg" src="" alt="image from file" />` +
-                    `</a>` +
-                    `<div class="px-5 pb-5">` +
-                    `<h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${results[i].vicinity}</h5>` +
-                    `<div class="flex items-center mt-2.5 mb-5">` +
-                    `<svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>` +
-                    `<svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>` +
-                    `<svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>` +
-                    `<svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>` +
-                    `<svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>` +
-                    `<span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">${results[i].rating}</span>` +
-                    `</div>` +
-                    `<span class="text-3xl font-bold text-gray-900 dark:text-white">${results[i].name}</span>` +
-                    `</div>` +
-                    `</div>` +
-                    `</div>` +
-                    `</div>`
+            siteDeck += `<div class="card w-96 glass">`+
+    `<figure><img src="/assets/img/shutterstock_1810380595.jpg" alt="car!"></figure>`+
+    `<div class="card-body">`+
+    `<span class="text-3xl font-bold text-gray-900 dark:text-white">${results[i].name}</span>` +
+        `<h2 class="card-title">${results[i].vicinity}</h2>`+
+        `<div class="card-actions justify-end">`+
+        `<div class="rating">`+
+  `<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400">`+
+  `<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked>`+
+  `<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400">`+
+  `<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400">`+
+  `<input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400">`+
+    `</div>`+
+        `<span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">${results[i].rating}</span>` +
+        `<button class="btn btn-primary">Go Now!</button>` +
+
+        `</div>`+
+    `</div>`+
+    `</div>`
+    
     }
     document.getElementById('happyTrails').innerHTML += siteDeck;     
-};
-    
 
-
-
-
-
-
-
+}
 
 
 
